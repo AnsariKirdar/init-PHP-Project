e15a2bea7da8a6d3dd5484bfae28b82f9789f9ce
AnsariKirdar <33788641+AnsariKirdar@users.noreply.github.com>
2021-04-10 00:20:52 +0530

    1

--- inc/footer.php
+++ inc/footer.php
@@ -1,1 +1,39 @@
-
+ <footer class="bg-dark">
+   <p class="text-center fw-bold small text-white">
+     © 2020 Vision. All rights reserved.
+   </p>
+   <p>
+   <ul class="nav">
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://www.facebook.com/visiontechnologymumbai"><img src="http://visiontechnology.co.in//img/common/icon/facebook.svg" alt="Vision Technology Facebook page" width="22px"></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://www.instagram.com/visiontechnologymumbai/?hl=en"><img src="http://visiontechnology.co.in//img/common/icon/insta.svg" alt="Vision Technology Linkedin Handle" width="22px"></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://wa.me/918850537174"><img src="http://visiontechnology.co.in//img/common/icon/whatsapp.svg" alt="Vision Technology Whatsapp Handle" width="22px"></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://twitter.com/Kirdarahmed"><img src="http://visiontechnology.co.in//img/common/icon/twitter.svg" alt="Vision Technology Twitter Handle" width="22px"></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://in.pinterest.com/ansarikirdar"><img src="http://visiontechnology.co.in//img/common/icon/pinterest.svg" alt="Vision Technology Twitter Handle" width="22px"></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://www.linkedin.com/in/ansarikirdar/"><img src="http://visiontechnology.co.in//img/common/icon/linkedln.svg" alt="Vision Technology Twitter Handle" width="22px"></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://www.youtube.com/channel/UCSVikbpWTnyQ8HnNqp0jizA"><img src="http://visiontechnology.co.in//img/common/icon/youtube.svg" alt="Vision Technology Twitter Handle" width="22px"></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://www.behance.net/ansarikirdar1"><img src="http://visiontechnology.co.in//img/common/icon/behance.svg" alt="Vision Technology Twitter Handle" width="22px"></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://dribbble.com/ansari-kirdar"><img src="http://visiontechnology.co.in//img/common/icon/dribble.svg" alt="Vision Technology Twitter Handle" width="22px"></a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link text-white fw-normal px-2" target="_blank" href="https://github.com/ansari-kirdar"><img src="http://visiontechnology.co.in//img/common/icon/git.svg" alt="Vision Technology Twitter Handle" width="22px"></a>
+     </li>
+   </ul>
+   </p>
+ </footer>
